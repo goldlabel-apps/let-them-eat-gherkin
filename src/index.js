af60d81ca066df91bde50aa963d3796a9ddf985e
weizang-75 <contact@wei-zang.com>
2019-06-03 20:15:19 +1000

    react(init): create-react-app and material ui

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tennis from './Tennis/Tennis';
+
+ReactDOM.render(
+    <React.Fragment>
+        <Tennis />
+    </React.Fragment>
+, document.getElementById('root'));
